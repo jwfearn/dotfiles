f2c698373002e803d6915f5d1431c96a8e70043b
jwfearn <jwfearn@gmail.com>
2014-04-21 07:40:59 -0700

    Latest work

--- install.cmd
+++ install.cmd
@@ -1,72 +1,80 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set H=%HOMEPATH%
-set DOTS=%H%\dotfiles
-REM Create a (local) timestamp, format: YYYYMMDDHHMMSS.
-call :timestamp T
-set BAK=%DOTS%_backup_%T%
-set EXCLUDES=".gitignore README.md install.bat install.sh"
+goto :start
 
-pushd %DOTS%
+:main
+  setlocal
+  set H=%HOMEPATH%
+  set DOTS=%H%\dotfiles
+  REM Create a (local) timestamp, format: YYYYMMDDHHMMSS.
+  call :timestamp T
+  set BAK=%DOTS%_backup_%T%
+  set EXCLUDES=".gitignore README.md install.bat install.sh"
 
-REM Get top-level non-ignored file and directory basenames.
-for /f %%f in ('git ls-tree --full-tree --name-only HEAD') do (
-  REM If not excluded
-  echo %EXCLUDES% | findstr "%%f" > nul & if errorlevel 1 (
-    REM If found in home directory
-    if exist "%H%\%%f" (
-      echo "WOULD DO: move %H%\%%f %BAK%\%%f"
-      REM TODO: If not a link, back it up
-      rem call :islink "%H%\%%f" L
-      rem echo "%L%"
+  pushd %DOTS%
+
+  REM Get top-level non-ignored file and directory basenames.
+  for /f %%f in ('git ls-tree --full-tree --name-only HEAD') do (
+    REM If not excluded
+    echo %EXCLUDES% | findstr "%%f" > nul & if errorlevel 1 (
+      REM If found in home directory
+      if exist "%H%\%%f" (
+        echo "WOULD DO: move %H%\%%f %BAK%\%%f"
+        REM TODO: If not a link, back it up
+        rem call :islink "%H%\%%f" L
+        rem echo "%L%"
+      )
     )
   )
-)
 
-popd
-endlocal
+  popd
+  endlocal
 goto:EOF
 
-
-
-REM Subroutines
 
 :timestamp
-setlocal
-for /f "tokens=1-3 delims=/:." %%a in ("%TIME%") do (set ret=%%a%%b%%c)
-for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set ret=%%c%%a%%b%ret: =0%)
-endlocal & if "%~1" neq "" set "%~1=%ret%"
+  setlocal
+  for /f "tokens=1-3 delims=/:." %%a in ("%TIME%") do (set t=%%a%%b%%c)
+  for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set t=%%c%%a%%b%t: =0%)
+  endlocal & if "%~1" neq "" set "%~1=%t%"
 goto:EOF
 
 
-:ln_s
-setlocal
-set opt=""
-REM Add /D option if target is a directory
-pushd %2 2> nul && set opt="/D" & popd
-mklink %opt% %1 %2
-endlocal
+:symlink
+  setlocal
+  set name=%1
+  set target=%2
+  set opt=""
+  REM Add /D option if target is a directory
+  pushd %target 2> nul && set opt="/D" & popd
+  mklink %opt% %name %target
+  endlocal
 goto:EOF
 
 
 :islink
-setlocal
-for %%i in ("%1") do set (attribs=%%~ai)
-echo attribs: %attribs%
-set ret="1"
-endlocal & if "%~2" neq "" set "%~2=%ret%"
+  setlocal
+  for %%i in ("%1") do set (attribs=%%~ai)
+  echo attribs: %attribs%
+  set ret="1"
+  endlocal & if "%~2" neq "" set "%~2=%ret%"
 goto:EOF
 
 
 :xislink
-setlocal
-for %i in ("%1") do set attribs=%~ai
-if "%attribs:~-1%" == "l" (set L=1) else (set L=0)
-endlocal & if "%~2" neq "" set "%~2=%T%"
+  setlocal
+  for %i in ("%1") do set attribs=%~ai
+  if "%attribs:~-1%" == "l" (set L=1) else (set L=0)
+  endlocal & if "%~2" neq "" set "%~2=%T%"
 goto:EOF
 
 
+:start
+call :main
+
+REM ===========================================================================
+REM  scratchpad
 goto:EOF
       call :islink "%H%\%%f" L
       echo "%L%    %H%\%%f"
--- install.sh
+++ install.sh
@@ -3,36 +3,67 @@ set -o nounset
 set -o errexit
 
 
-H=~
-DOTS=${H}/dotfiles
-# Create a (local) timestamp, format: YYYYMMDDHHMMSS.
-T=$(date +%Y%m%d%H%M%S)
-BAK=${DOTS}_backup_${T}
-EXCLUDES=".gitignore README.md install.bat install.sh"
+function main() {
 
-pushd $DOTS
+  local H=~
+  local DOTS=${H}/dotfiles
+  # Create a (local) timestamp, format: YYYYMMDDHHMMSS.
+#  local T=$(date +%Y%m%d%H%M%S)
+  timestamp T
+  local BAK=${DOTS}_backup_${T}
+  local EXCLUDES=".gitignore README.md install.bat install.sh"
 
-# Get top-level non-ignored file and directory basenames.
-for f in $(git ls-tree --full-tree --name-only HEAD); do
-  # If not excluded
-  if [[ "${EXCLUDES##*$f*}" ]]; then
-    # If found in home directory
-    if [[ -e "$H/$f" ]]; then
-#      echo "WOULD DO: mkdir -p $BAK"
-      echo "WOULD DO: mv $H/$f $BAK/$f"
-#     TODO: If not a link, back it up
+  pushd $DOTS
+
+  # Get top-level non-ignored file and directory basenames.
+  for f in $(git ls-tree --full-tree --name-only HEAD); do
+    # If not excluded
+    if [[ "${EXCLUDES##*$f*}" ]]; then
+      # If found in home directory
+      if [[ -e "$H/$f" ]]; then
+  #      echo "WOULD DO: mkdir -p $BAK"
+        echo "WOULD DO: mv $H/$f $BAK/$f"
+  #     TODO: If not a link, back it up
+      fi
+  #    echo $f
+  #  else
+  #    echo EXCLUDED: $f
     fi
-#    echo $f
-#  else
-#    echo EXCLUDED: $f
+  #  echo "WOULD DO: ln -s $H/$f $DOTS/$f" # TODO: overwrite existing file
+  done
+
+  popd
+}
+
+function timestamp {
+  local t=$(date +%Y%m%d%H%M%S)
+  ${$1}=t
+}
+
+function symlink {
+  local name=xxx
+  local target=xxx
+  $(ln -s name target)
+}
+
+function backup {
+  # Copy f (relative to working directory) to d (absolute path), creating d
+  # if it doesn't exist.  Do nothing if f is a link.
+  local basename=xxx
+  local d=xxx
+  local ok=0
+  if [[ ok ]]; then
+    mkdir -p d
+    echo "WOULD DO: mv $H/$f $d/$f"
   fi
-#  echo "WOULD DO: ln -s $H/$f $DOTS/$f" # TODO: overwrite existing file
-done
+}
 
-popd
+function islink {
+  local f=xxx
+}
 
 
-
+main
 
 
 # TODO: for each non-git-ignored file or directory
