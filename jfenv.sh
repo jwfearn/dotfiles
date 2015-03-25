c774095030718dbea4f62682440e81e7e8492051
jwfearn <jwfearn@gmail.com>
2015-03-24 19:35:23 -0700

    Don't set GREP_OPTIONS; braces

--- jfenv.sh
+++ jfenv.sh
@@ -5,28 +5,27 @@
 
 # environment variables used by multiple shells
 
-# export LINKSCAPE_REPO_ROOT=$HOME/github/linkscape
-# export PATH=JFENV-:/usr/local/Cellar/gnu-getopt/1.1.5/bin:$PATH:$HOME/bin:/usr/local/mysql/bin:/usr/local/packer:-JFENV
+# export LINKSCAPE_REPO_ROOT=${HOME}/github/linkscape
+# export PATH=JFENV-:/usr/local/Cellar/gnu-getopt/1.1.5/bin:$PATH:${HOME}/bin:/usr/local/mysql/bin:/usr/local/packer:-JFENV
 export PATH="JFENV-:${PATH}:${HOME}/bin:/usr/local/mysql/bin:/usr/local/packer:-JFENV"
 export EDITOR='subl -w'
 export CLICOLOR=YES
 #export MOZOO_PATH=~/github/mozoo (moved to launchd.conf to support RM)
-export GREP_OPTIONS='--color=auto'
-export DOTFILES="${HOME}/dotfiles"
+#export GREP_OPTIONS='--color=auto'
 
 # pip should only run if there is a virtualenv currently activated
 # export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
+export PIP_DOWNLOAD_CACHE="${HOME}/.pip/cache"
 
 # export LS_COLORS='' # Linux
 # export LSCOLORS='' # OS X,
 export CLICOLOR=1 # use colors in supported commands (ls, others?)
 
 ## Secrets!
-export EC2_CERT="$HOME/.ssh/bigdata20140401/bigdata20140401.cert"  # deprecated?
-export EC2_PRIVATE_KEY="$HOME/.ssh/bigdata20140401/bigdata-20140401.pk"  # deprecated?
-export MOZSCAPE_KEY="$HOME/.ssh/bigdata20140401/bigdata-20140401.pem"
+export EC2_CERT="${HOME}/.ssh/bigdata20140401/bigdata20140401.cert"  # deprecated?
+export EC2_PRIVATE_KEY="${HOME}/.ssh/bigdata20140401/bigdata-20140401.pk"  # deprecated?
+export MOZSCAPE_KEY="${HOME}/.ssh/bigdata20140401/bigdata-20140401.pem"
 
 . "${HOME}/github/linkscape/scripts/secrets.sh"
 # environment variables for ec2-api-tools
