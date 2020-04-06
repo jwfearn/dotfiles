693b2d3aba56e0a24bb2756f52d3f5d1e21b8806
John Fearnside <fearnsid@adobe.com>
2020-04-05 18:24:14 -0700

    Remove unused code, use ls aliases

--- jffn.sh
+++ jffn.sh
@@ -9,6 +9,9 @@ alias 'cd....'='cd ../../..'
 alias 'cd.....'='cd ../../../..'
 alias 'cd-'='cd -'
 alias 'wat'="git for-each-ref --count=30 --sort=committerdate refs/head/ --format='%(refname:short)' | fzf | xargs git checkout"
+alias 'ls'='ls -haF'
+alias 'll'='ls -l'
+# l() { ls "$@"; }
 
 # Version 0.9.4 is available! (You are running version 0.9.3) Please download our latest version.
 op_() { which op > /dev/null && op "$@"; }
@@ -122,10 +125,6 @@ whilefail() { while ! $1 ; do :; done; }
 
 vt() { TESTOPTS='--verbose' t "$@"; }
 
-# ls() { env ls -haF "$@"; }
-# l() { ls "$@"; }
-# ll() { ls -l "$@"; }
-
 man() {
   env \
     LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
@@ -138,85 +137,6 @@ man() {
     env man "$@"
 }
 
-# prt() { lsof -n -i4TCP:$1 | grep LISTEN; }
-
-## Linkscape-related functions
-# alias sx_="echo url{1..10} | tr ' ' '\n' | xargs -P10 -I^ ssh ^ "
-# alias sxk_="echo url{1..10} | tr ' ' '\n' | xargs -I^ ssh ^ "
-# schk() { sx_ 'echo "${HOSTNAME}: jshd: $(pgrep -c jshd), script: $(pgrep -c start_urlsched)  $(cd /mnt/cluster/bh/work && ls -C STOP Stop Quit Restart Start 2> /dev/null)"'; }
-# sstop() { sx_   ; }
-# sstart() { sx_ 'cd /mnt/cluster/bh/work && ./start_crawler'; }
-# srestart() { sx_ 'cd /mnt/cluster/bh/work && touch Restart && ./start_crawler'; }
-# sxf_() { parallel --no-notice -k --bar "parallel --no-notice -S url{} -n0 --transfer ::: 1" ::: {1..10}; }
-# sxf_() { local f=$1; shift; parallel --no-notice "$@" "parallel --no-notice -S 1/url{} --transfer --cleanup bash ::: $f" ::: {1..10}; }
-# cxf_() { local f=$1; shift; parallel --no-notice -k "$@" "parallel --no-notice -S 1/crawl{} --transfer --cleanup bash ::: $f" ::: {1..20}; }
-# sxfn_() { local fn=$1 && export -f $fn && parallel --no-notice -k --bar "parallel --no-notice -S 1/url{} -n0 --env $fn $fn ::: 1" ::: {1..10}; }
-# cxfn_() { local fn=$1 && export -f $fn && parallel --no-notice -k --bar "parallel --no-notice -S 1/crawl{} -n0 --env $fn $fn ::: 1" ::: {1..20}; }
-# cck() { date; cxfn_ cstat; }
-# sck() { date; sxfn_ sstat; }
-# cck() { date; pushd . > /dev/null; cdl; cxf_ scripts/cstat.sh "$@"; popd > /dev/null; }
-# sck() { date; pushd . > /dev/null; cdl; sxf_ scripts/sstat.sh "$@"; popd > /dev/null; }
-# cck() { cdl && scripts/crawlers_status "$@"; }
-# sck() { cdl && scripts/schedulers_status "$@"; }
-# findbad() {
-#   ls -Sr *.lzoc | head -5 | xargs -P$(($(nproc) - 1)) -I^ ../bin/copystream ^ - >& /dev/null && echo 'GOOD: ^' || echo 'BAD: ^';
-# }
-# alias cx_="echo crawl{1..20} | tr ' ' '\n' | xargs -P20 -I^ ssh ^ "
-# alias cxk_="echo crawl{1..20} | tr ' ' '\n' | xargs -I^ ssh ^ "
-# alias tx_="echo ctest{A..C} | tr ' ' '\n' | xargs -P10 -I^ ssh ^ "
-# alias txk_="echo ctest{A..C} | tr ' ' '\n' | xargs -I^ ssh ^ "
-
-# JF_VERSIONS_CMD=' \
-#     echo ^: \
-#     $(lsb_release -sd), \
-#     $(gcc --version | head -1), \
-#     $(grep BOOST_LIB_VERSION /usr/include/boost/version.hpp | tail -1) \
-# '
-#     # $(git --version), \
-#     # $(bash --version | head -1), \
-#     # $(make --version | head -1), \
-#     # $(gdb --version 2> /dev/null | head -1) \
-
-# tcc() { txk_ "${JF_VERSIONS_CMD}"; }
-# ccc() { cxk_ "${JF_VERSIONS_CMD}"; }
-# scc() { sxk_ "${JF_VERSIONS_CMD}"; }
-
-# cpi() { cxk_ 'echo ^: $(initctl status procinfo >& | xargs)'; }
-# spi() { sxk_ 'echo ^: $(initctl status procinfo >& | xargs)'; }
-# cpyenvs() { cxk_ 'echo ^: $(ls -d /mnt/cluster/bh/work/.pyenv/bin ~/.pyenv/bin 2> /dev/null)'; }
-# spyenvs() { sxk_ 'echo ^: $(ls -d /mnt/cluster/bh/work/.pyenv/bin ~/.pyenv/bin 2> /dev/null)'; }
-# alias cxs_="echo crawl{1..20} | tr ' ' '\n' | xargs -I^ ssh ^ "
-# cchk() { date; cx_ 'echo "${HOSTNAME}: jshd: $(pgrep -c jshd), script: $(pgrep -c start_crawler), async-dirs: $(ls -ld /mnt/cluster/bh/work/Async-* 2> /dev/null | wc -l), async-ps: $(ps -ef | grep -v grep | grep crawlv2loop | grep Async | wc -l)  $(cd /mnt/cluster/bh/work && ls -C STOP Stop Quit Restart Start 2> /dev/null)"'; }
-# cchk() { date; cx_ 'echo "${HOSTNAME}: jshd: $(pgrep -c jshd), script: $(pgrep -c start_crawler), async-dirs: $(find Async-* -maxdepth 0 -type d -print 2> /dev/null | wc -l), async-ps: $(ps -ef | grep -v grep | grep crawlv2loop | grep Async | wc -l)  $(cd /mnt/cluster/bh/work && ls -C STOP Stop Quit Restart Start 2> /dev/null)"'; }
-# cchk() { cx_ 'echo "${HOSTNAME}: jshd: $(pgrep -c jshd), script: $(pgrep -c start_crawler)"'; }
-# casynp() { cx_ 'a=$(ls -d /mnt/cluster/bh/work/Asy* 2>/dev/null | wc -l);echo "^ $a"'; }
-# cstop() { cx_ 'touch /mnt/cluster/bh/work/Stop'; }
-# cstart() { cx_ 'cd /mnt/cluster/bh/work && ./start_crawler'; }
-# lln() { cx_ 'cd /mnt/cluster/bh/bin && ln -s ../src/release/processing/canonicalize .'; }
-# lbr() { cx_ 'cd /mnt/cluster/bh/src && git branch'; }
-# alias fx_="echo dfix{0..9} | tr ' ' '\n' | xargs -P10 -I^ ssh ^ "
-# fxpull() { fx_ 'cd /mnt/cluster/bh/src && git checkout SAVANNA-2737-transfer-fix-script && git pull'; }
-#  echo fixer{0..9} | tr ' ' '\n' | xargs -P10 -I^ scp root@^:/mnt/log/fixtransfers_*.log ./^logs/
-# cps() { cd ~/repos/linkscape/scripts/ && scp steps.py requirements.txt va:/mnt/cluster/bh/src/scripts/; }
-# cps() { echo crawl{1..20} | tr ' ' '\n' | xargs -P20 -I^ scp ~/Save ^:~;  }
-# cps() { cd ~/repos/linkscape/scripts/ && scp steps.py requirements.txt crawl1:/mnt/cluster/bh/src/scripts/; }
-
-# filterpath() {
-#   local readonly oldpath="${PATH}"
-#   local readonly newpath=oldpath
-#   export PATH="${newpath}"
-# }
-
-# abspath() {
-#   local readonly p="${1}"
-#   local absp=''
-#   # if [ -f "${p}" ]; then
-#   #   absp="$(cd $(dirname "${p}") && pwd)/$(basename "${p}")"
-#   # elif [ -d "${p}" ]; then
-#   #   absp="$(cd "${p}" && pwd)"
-#   # fi
-#   echo "${absp}"
-# }
 
 pathpp() {
   saveIFS="${IFS}"
@@ -314,7 +234,7 @@ ei() { env | sort | grep "$@"; }
 e() { subl "$@"; }
 h() { history | tail -"$1"; }
 history_remove_all() { rm "${HISTFILE}"; }
-history_remove_session() { { local HISTSIZE=0; }
+history_remove_session() { local HISTSIZE=0; }
 lsl() { env ls -lOeF "$@"; }  # -O is a Mac-specific option
 lsd() { env ls -d .*/ "$@"; }
 lsapts() {
@@ -360,35 +280,6 @@ gdiff() { git diff -U0 --no-index "$@"; }
 mktags() { mkdir -p ./.tags; ctags -R -f ./.tags/tags "$@" .; }
 makels() { make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' | sort | uniq; }
 csha() { curl -fSL "$@" | shasum -a 256 --; }
-
-# modo() {}  # TODO: get permissions as octal string
-
-
-# `ps -ef` header row:
-# UID        PID  PPID  C STIME TTY          TIME CMD
-# `ps -ely` header row:
-# S   UID   PID  PPID  C PRI  NI   RSS    SZ WCHAN  TTY          TIME CMD
-#
-#   S = minimal state display (one of: DRSTWXZ)
-#   UID = effective user ID
-#   PID = process ID
-#   PPID = parent process ID
-#   C = processor utilization integer
-#   PRI = priority, higher number means lower priority
-#   NI = nice value. This ranges from 19 (nicest) to -20 (not nice to others)
-#   RSS = resident set size (KB), non-swapped physical memory task has used
-#   SZ = approximate amount of swap space
-#   WCHAN = address of the kernel where the process is sleeping
-#   STIME = xxx
-#   TTY = terminal associated with process
-#   TIME = cumulative CPU time, "[dd-]hh:mm:ss" format.
-#   CMD = command name (only the executable name).
-#
-# `ps -ef` example data row:
-# martin     379 28712  0 Feb24 pts/3    00:00:01 mysql -u Moz -p Moz
-# `ps -ely` example data row:
-# S  1000  3764  3348  0  80   0  3208 19739 poll_s pts/3    00:00:00 jshd
-
 
 ## OS X-specific commands
 cpus() { sysctl -n hw.ncpu; }
@@ -582,17 +473,6 @@ cdt() { cd '/Users/john/repos/other/release_training_elixirconf_2019/2_configuri
 pgi() { cdi; psql -W -U postgres termfront_dev "$@"; }
 psqld() { psql --host=localhost --port=5432 "$@"; }
 
-## BEGIN: SeatEngine-related functions
-cdw() { pushd_ "${SEAT_ENGINE_DOCKER}"; }
-cdc() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-core"; }
-cde() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-elixir"; }
-cdp() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-payments"; }
-cdf() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-print"; }
-cds() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-shows"; }
-cdu() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-users"; }
-cdv() { pushd_ "${SEAT_ENGINE_DOCKER}/repos/seat-engine-venues"; }
-## END: SeatEngine-related functions
-
 ## BEGIN: Avvo-related functions
 agems() {
   gem list \
@@ -681,6 +561,7 @@ doki() {
   npm run compile-tests
   bundle exec foreman start
 }
+
 
 ## Bundler-related functions
 bcinfo() { cat ~/.bundle/config 2> /dev/null; cat .bundle/config 2> /dev/null; }
@@ -801,33 +682,6 @@ pip3_() { which pip3 > /dev/null && pip3 "$@"; }
 pps() { pip3_ list "$@"; }
 ppo() { pip3_ list --outdated "$@"; return 0; }
 ppu() { pip3_ list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U; }
-
-# As of 17 Jan 2020, pipupgrade is buggy
-# pipupgrade3_() { pipupgrade --pip-path '/usr/local/bin/pip3' "$@"; }
-# ppo() { pipupgrade3_ --check "$@"; }
-# ppu() { pipupgrade3_ --self --yes && pipupgrade3_ --latest --yes "$@"; }
-
-# pys() { pyenv_ --version && pyenv_ versions; echo "CURRENT PYTHON: $(python_ --version 2>&1)"; }
-# pyis_() { pyenv_ install --list "$@"; }
-# pyis() { pyis_ | column; }
-# py+() { pyenv_ install "$1"; }
-# py-() { pyenv_ uninstall "$1"; }
-# pyup() { pyis_ > pyis0.txt; brew upgrade pyenv; pyis_ > pyis1.txt; gdiff pyis0.txt pyis1.txt; }
-# py0() { pyenv_ local system; pys; }
-# py2() { pyenv_ local 2.7.17; pys; }
-# py3() { pyenv_ local 3.8.0; pys; }
-# pyb() { pyenv_ local 3.7.5-brew; pys; }
-# pya() { pyenv_ local anaconda3-5.0.1; pys; }
-# xxppu() { pyenv_ pip-update "$@"; }
-# xppu() { which pip > /dev/null && pip freeze --local | sed -En 's/^([^=# \t\\][^ \t=]*)=.*/echo; echo Processing \1 ...; pip install -U \1/p' | sh; }
-
-#pyl() { pyenv local linkscape; }
-# syspip() { PIP_REQUIRE_VIRTUALENV='' pip_ "$@"; }
-# pyx() { local cmd="$@"; local py="from subprocess import check_output as x; o = x('$cmd'); print(o)"; echo $py; python_ -c"$py"; }
-# pea() { pyenv_ activate "$@"; }
-# ped() { pyenv_ deactivate "$@"; }
-# pyv() { pyenv_ virtualenv "$@"; }
-
 
 
 ## MySQL-related functions
@@ -1006,42 +860,6 @@ zshi() {
   zsh --version
   filesp_ "$(zshprofiles_)"
 }
-
-## Vanguard-related functions
-# vcredx_() { cdv && cd config && mv -v credentials.development.yml credentials.test.yml ${HOME}/_out; }
-# vcredcp_() { cp -v credentials.development.yml credentials.test.yml; }
-# vcred() { vcredx_ && cp -v ${HOME}/Downloads/credentials.development.yml . && vcredcp_; }
-# ## Vanguard test manual prerequisites: 1) mysql running, 2) drop local vanguard* schemas, 3) redis-server running
-# vun_() { echo UNIT TESTS; bspec spec/unit; }
-# vun() { vun_; }
-# vun_full() { echo UNIT PREREQUISITES; cdv && bi && br db:recreate_master && vun_; }
-#
-# vin_() { echo INTEGRATION TESTS; bspec spec/integration; }
-# vin() { vun && vin_; }
-# vin_full() { vun_full && vin_; }
-#
-# vsc_() { echo SHARD CREATION TESTS; ulimit -Sn 2048 && bspec spec/acceptance/shard_creation_spec.rb; }
-# vsc() { vin && vsc_; }
-# vsc_full() { vin_full && vsc_; }
-#
-# vac_() { echo ACCEPTANCE TESTS; br spec:server:acceptance; }
-# vac() { vsc && vac_; }
-# vac_full() { vsc_full && vac_; }
-
-## Oyez-related aliases
-# omi() { bx sequel -m db/migrate mysql2://root:crash19spit@localhost/oyez_development; }
-# odu() { unicorn -d; }
-# disabled aliases
-# alias fixdir='ruby ${HOME}/bin/fixdir.rb' # fixes unzipped Closure Library files
-# alias reclone='ruby ${HOME}/bin/reclone.rb'
-# alias tp='ruby ${HOME}/bin/testprj.rb'
-# alias tpl='ruby ${HOME}/bin/testprjlocal.rb'
-# alias plovr='java -jar ${HOME}/bin/plovr-4b3caf2b7d84.jar'
-# alias cb='python ${HOME}/closure-library-read-only/closure/bin/build/closurebuilder.py'
-# alias dw='python ${HOME}/closure-library-read-only/closure/bin/build/depswriter.py'
-# alias rmpid='rm ${HOME}/repos/freya/tmp/pids/vanguard_stub_app.pid'
-# alias cdlb='cd ${HOME}/FOWLER/work/tech/external/lb/lbfork'
-
 
 rc() { e "${DOTFILES}/jffn.sh"; }
 src() { . "${DOTFILES}/jffn.sh"; }  # reload function only
